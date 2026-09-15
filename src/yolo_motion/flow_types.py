19f2cba497b8669b91d68093d742a47dbafe8d7a
Qi Gao <qi.gao2012@gmail.com>
2026-09-15 17:49:56 +0800

    style: satisfy optical gait contract lint

--- src/yolo_motion/flow_types.py
+++ src/yolo_motion/flow_types.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import math
+from collections.abc import Mapping
 from dataclasses import dataclass
 from types import MappingProxyType
-from typing import Mapping
 
 import numpy as np
 
